1c52a88a8310e2575717b85ee8b550bec06d34a2
unknown <chahb021@gmail.com>
2022-12-17 06:36:12 +0500

    Updates

---
+++ precache-manifest.7488fc8f994a8779083b31d01eac8f17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c866d4d87bfaaee1edf4",
+    "url": "/sapp/static/css/main.1a053835.chunk.css"
+  },
+  {
+    "revision": "c866d4d87bfaaee1edf4",
+    "url": "/sapp/static/js/main.c866d4d8.chunk.js"
+  },
+  {
+    "revision": "43fe12da28975d384c88",
+    "url": "/sapp/static/js/1.43fe12da.chunk.js"
+  },
+  {
+    "revision": "1df0eadf5f6debb02ccc",
+    "url": "/sapp/static/js/runtime~main.1df0eadf.js"
+  },
+  {
+    "revision": "e6af6c4c14dde6381246ae60145cc5f2",
+    "url": "/sapp/index.html"
+  }
+];
